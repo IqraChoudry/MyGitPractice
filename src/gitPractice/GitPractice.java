ffbf7af77247b6c943de4f9ecc9bd5dde79ca700
Iqra <iqrachoudry92@gmail.com>
2022-03-21 00:35:54 -0400

    Iqraaaaaaaaaaaaaaa

--- src/gitPractice/GitPractice.java
+++ src/gitPractice/GitPractice.java
@@ -6,10 +6,11 @@ public class GitPractice {
 
 			System.out.println(" My Name is TalenTech");
 			System.out.println( "Whats your name?");
+			System.out.println(" My Name is Iqraaaaaaaaaaaa!");
+			System.out.println(" My Name is Iqraaaaaaaaaaaaaa!");
 			System.out.println(" My Name is Iqraaaaaaaaaaaaaaa!");
-		        System.out.println(" My Name is Iqraaaaaaaaaaaaaaa!");
-		System.out.println(" My Name is Iqraaaaaaaaaaaaaaa!");
-		        System.out.println(" My Name is Iqraaaaaaaaaaaaaaa!");
+			System.out.println("aaaaaaaaaa");
+			
 	}
 
 }
