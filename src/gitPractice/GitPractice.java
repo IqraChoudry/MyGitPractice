99888add67588ac032b8e389c644209ded4b7291
Iqra <iqrachoudry92@gmail.com>
2022-03-20 22:57:54 -0400

    My name 4 times

--- src/gitPractice/GitPractice.java
+++ src/gitPractice/GitPractice.java
@@ -7,6 +7,9 @@ public class GitPractice {
 			System.out.println(" My Name is TalenTech");
 			System.out.println( "Whats your name?");
 			System.out.println(" My Name is Iqraaaaaaaaaaaaa!");
+			System.out.println(" My Name is Iqraaaaaaaaaaaaa!");
+			System.out.println(" My Name is Iqraaaaaaaaaaaaa!");
+			System.out.println(" My Name is Iqraaaaaaaaaaaaa!");
 			System.out.println();
 			
 	}
