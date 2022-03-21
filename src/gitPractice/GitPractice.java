caf822560570d5b59a8deb334d3782f5be77e046
Iqra <iqrachoudry92@gmail.com>
2022-03-20 22:23:44 -0400

    Git Ignore updated, Git Practice updated

--- src/gitPractice/GitPractice.java
+++ src/gitPractice/GitPractice.java
@@ -3,8 +3,9 @@ package gitPractice;
 public class GitPractice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+			System.out.println(" My Name is TalenTech");
+			System.out.println( "Whats your name?");
 	}
 
 }
